11367d1439512310e9be332c825740484c011280
abel-hak <hakensoabel@gmail.com>
2025-02-05 10:01:53 +0300

    Created 'Presents' by A2SV Hub

---
+++ 05-Feb-2025/Presents 242150.py
@@ -0,0 +1,11 @@
+# Problem: Presents - https://codeforces.com/problemset/problem/136/A
+
+n = int(input()) 
+p = list(map(int, input().split())) 
+
+result = [0] * n 
+
+for i in range(n):
+    result[p[i] - 1] = i + 1  
+
+print(" ".join(map(str, result)))
