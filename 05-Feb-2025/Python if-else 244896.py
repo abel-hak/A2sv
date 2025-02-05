7c2dfe49a1a6c861781036962978446af743b955
abel-hak <hakensoabel@gmail.com>
2025-02-05 21:50:28 +0300

    Created 'Python if-else' by A2SV Hub

---
+++ 05-Feb-2025/Python if-else 244896.py
@@ -0,0 +1,23 @@
+# Problem: Python if-else - https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    if n % 2 == 1:
+        print("Weird")
+    elif 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
