82fc3cbec6840f2e04cf08aa63433f9df4c267da
abel-hak <hakensoabel@gmail.com>
2025-02-07 09:55:27 +0300

    Created 'Flipping an Image' by A2SV Hub

---
+++ 07-Feb-2025/Flipping an Image 248991.py
@@ -0,0 +1,16 @@
+# Problem: Flipping an Image - https://leetcode.com/problems/flipping-an-image/description/
+
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        ans = []
+        for i in image:
+            ans.append(i[::-1])
+        image = ans
+        for i in range(len(image)):
+            for j in range(len(image[0])):
+                if image[i][j] == 0:
+                    image[i][j] = 1
+                else:
+                    image[i][j] = 0
+
+        return image
