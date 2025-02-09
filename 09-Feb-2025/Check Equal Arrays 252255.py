397d9462f04fdd320a6cd263cb2d0f2356724ed6
abel-hak <hakensoabel@gmail.com>
2025-02-09 20:38:23 +0300

    Created 'Check Equal Arrays' by A2SV Hub

---
+++ 09-Feb-2025/Check Equal Arrays 252255.py
@@ -0,0 +1,13 @@
+# Problem: Check Equal Arrays - https://practice.geeksforgeeks.org/problems/check-if-two-arrays-are-equal-or-not3847/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+
+
+class Solution:
+    # Function to check if two arrays are equal or not.
+    def checkEqual(self, a, b) -> bool:
+        #code here
+        a.sort()
+        b.sort()
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                return False
+        return True
