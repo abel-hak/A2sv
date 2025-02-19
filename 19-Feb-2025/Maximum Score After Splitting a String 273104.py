8e8884c02d88c5146965b3f8d9d2287fe3e998af
abel-hak <hakensoabel@gmail.com>
2025-02-19 10:55:16 +0300

    Created 'Maximum Score After Splitting a String' by A2SV Hub

---
+++ 19-Feb-2025/Maximum Score After Splitting a String 273104.py
@@ -0,0 +1,15 @@
+# Problem: Maximum Score After Splitting a String - https://leetcode.com/problems/maximum-score-after-splitting-a-string/
+
+class Solution:
+    def maxScore(self, s: str) -> int:
+        prefix = []
+        total = 0
+        for i in s:
+            total += int(i)
+            prefix.append(total)
+            
+        max1 = float('-inf')
+        for i in range(len(s) - 1):  
+            sum1 = prefix[-1] - prefix[i] + (i + 1 - prefix[i])
+            max1 = max(max1, sum1)
+        return max1
